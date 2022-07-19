18445c0f16f231579c82e9b17fa291c88cb9870c
tkornem <tkornem@gmail..com>
2022-07-19 10:04:48 -0500

    Error for loadError

--- shibim-core/src/error.rs
+++ shibim-core/src/error.rs
@@ -1,16 +1,17 @@
 use shibim_base::SHBParseError;
 use thiserror::Error;
 
-
+#[derive(Error,Debug)]
+#[error("Error loading file '{file}'")]
 pub struct LoadError{
     pub file : String,
-
+    #[source]
     pub detail : LoadErrorPayload
 }
 #[derive(Error,Debug)]
 pub enum LoadErrorPayload{
     #[error(transparent)]
     IOError(#[from]std::io::Error),
-    #[error("Error parsing shb file '{0:?}'")] //Todo, this is duct tape
+    #[error("Synax error '{0:?}'")] //Todo, this is duct tape
     ParseError(Vec<SHBParseError>)
 }
