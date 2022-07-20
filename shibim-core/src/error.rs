bfa24b7971c0f11c4280a8bfc5a8f2e526394001
tkornem <tkornem@gmail..com>
2022-07-19 23:41:03 -0500

    working/exploring architecture of batch processing of files

--- shibim-core/src/error.rs
+++ shibim-core/src/error.rs
@@ -1,5 +1,6 @@
 use shibim_base::SHBParseError;
 use thiserror::Error;
+use std::path::PathBuf;
 
 #[derive(Error,Debug)]
 #[error("Error loading file '{file}'")]
@@ -8,10 +9,23 @@ pub struct LoadError{
     #[source]
     pub detail : LoadErrorPayload
 }
+
+#[derive(Error,Debug)]
+#[error("Post-processing error, {msg}")]
+pub struct VisitorError{
+    pub msg : String,
+    detail : Option<Box<dyn std::error::Error>>
+}
+
 #[derive(Error,Debug)]
 pub enum LoadErrorPayload{
     #[error(transparent)]
     IOError(#[from]std::io::Error),
     #[error("Synax error '{0:?}'")] //Todo, this is duct tape
     ParseError(Vec<SHBParseError>)
+}
+
+struct SessionInfo{
+    cur_file : Option<PathBuf>,
+    load_errors : Vec<LoadError>
 }
--- shibim-core/src/files.rs
+++ shibim-core/src/files.rs
@@ -1,8 +1,9 @@
 use std::fs as fs;
 use std::path::{Path};
-use crate::error::{LoadError, LoadErrorPayload};
+use crate::error::{LoadError, LoadErrorPayload, VisitorError};
 use shibim_base::*;
 use shibim_parse::parse_song;
+use std::collections::HashMap;
 
 fn read_shb(file_name : &Path) -> 
     Result<Song,LoadError >{
@@ -23,3 +24,28 @@ fn read_shb(file_name : &Path) ->
         )
     )
 }   
+
+fn read_shb_batch_seq<'a, I>(batch : I, visitors : Vec<&mut dyn SongVisitor>)
+-> HashMap<String,Song>
+where
+    I : IntoIterator<Item = &'a Path>{
+    let mut out = HashMap::new();
+    for file in batch{
+        match read_shb(file){
+            Ok(song) =>{
+                let err = out.insert(file.to_string_lossy().to_string(),song);
+                if err.is_some(){
+                    eprintln!("Files with same UTF-8 filename {}",file.display());
+                }
+            },
+            Err(err) =>{
+                eprintln!("{}",err);
+            }
+        }
+    }
+    out
+}
+
+trait SongVisitor{
+    fn process(&mut self, e : &Song) -> Result<(),VisitorError>;
+}
